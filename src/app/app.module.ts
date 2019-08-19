3e3bb6730425ad593382e5df7f2367bffcd6f16a
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-08-19 09:44:53 +0300

    Check and fix various spelling errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    QuoteComponent,
     QuoteFormComponent,
     QuoteDetailsComponent,
     DateCountPipe
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+    quotes = [
+      new Quote(1,"The Way Get Started Is To Quit Talking And Begin Doing.","Author:Walt Disney","Marcus-Jean-Louis",0,0,new Date(2019,8,18)),
+      new Quote(2,"The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.","Author:Albert Einstein","Marcus-Jean-Louis",0,0,new Date(2019,8,18)),
+      new Quote(3,"Don’t Let Yesterday Take Up Too Much Of Today.","Author:Will Rogers","Marcus-Jean-Louis",0,0,new Date(2019,8,18)),
+      new Quote(4,"You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.","Author:Unknown","Marcus-Jean-Louis",0,0,new Date(2019,8,18)),
+      new Quote(5,"It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.","Author:Vince Lombardi","Marcus-Jean-Louis",0,0,new Date(2019,8,18)),
+    ]
 
-  ngOnInit() {
-  }
+    toogleDetails(index){
+   this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
+   this.quotes[index].showSubmit= !this.quotes[index].showSubmit;
+ }
+
+ upvote(index){
+ this.quotes[index].upvotes++;
+}
+downvote(index){
+ this.quotes[index].downvotes++;
+}
+deleteQuote(isComplete,index){
+ if (isComplete){
+   let toDelete=confirm(`Are you sure? ${this.quotes[index].name}`)
+   if(toDelete=true){
+     this.quotes.splice(index,1)
+   }
+}
+
+ }
+
+
+ addNewQuote(quote){
+  this.quotes.push(quote)
+
+ }
+
+ constructor() { }
+
+ ngOnInit() {
 
 }
+}
+
